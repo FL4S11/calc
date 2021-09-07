ea56076fe0fd26d90ebde9c096caee3c2693e455
wsruser1 <alekseev_lnt@mail.ru>
2021-09-07 14:24:23 +0300

    fix

--- Calculator FL/ViewController.swift
+++ Calculator FL/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var operand = ""
+    var numberOne = ""
+    var numberTwo = ""
+    
+
+    @IBOutlet weak var resultLabel: UILabel!
+    //Функция кнопок 0-9
+    @IBAction func inputNumber(_ sender: UIButton) {
+        if operand.isEmpty {
+            numberOne = numberOne + (sender.titleLabel?.text)!
+            resultLabel.text = numberOne
+        } else {
+            numberTwo = numberTwo + (sender.titleLabel?.text)!
+            resultLabel.text = numberTwo
+        }
     }
-
-
+    
+    // Функция обработки нажатия на операнды
+    @IBAction func inputOperand(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as! String
+    }
+    
+    // Кнопка результата
+    @IBAction func resultAction( sender: Any){
+        var result = 0.0
+        switch operand {
+        case "/":
+            result = Double(numberOne)! / Double(numberTwo)!
+        case "*":
+            result = Double(numberOne)! * Double(numberTwo)!
+        case "+":
+            result = Double(numberOne)! + Double(numberTwo)!
+        case "-":
+            result = Double(numberOne)! - Double(numberTwo)!
+        default:
+            break
+        }
+        if result.truncatingRemainder(dividingBy: 1.0) == 00 {
+            resultLabel.text = String(Int(result))
+        } else {
+            resultLabel.text = String(result)
+        }
+    }
+    
+    // Функция обнуления
+    @IBAction func clearAction(_ sender: UIButton) {
+        numberOne = ""
+        numberTwo = ""
+        operand = ""
+        resultLabel.text = "0"
+    }
 }
-
